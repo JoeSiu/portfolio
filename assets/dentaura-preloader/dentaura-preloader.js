44d2cfb7806f9336737cab25735b4865a3be6e93
Joe_Siu <JoeSiu@.github.com>
2021-02-11 13:49:01 +0800

    Update dentaura-preloader.js

--- assets/dentaura-preloader/dentaura-preloader.js
+++ assets/dentaura-preloader/dentaura-preloader.js
@@ -1,12 +1,28 @@
 (function ($) {
   'use strict';
 
-  // Preloader
-  $(window).on('load', function () {
+  //remove preloader
+  function removePreloader() {
     $('#loading-preloader')
       .delay(250)
       .fadeOut('slow', function () {
         $(this).remove();
       });
+  }
+
+  //remove preloader when skip loading link is clicked
+  $("#skip-loading-link").click(function(e){
+      removePreloader();
   });
+
+  //remove preloader when window is loaded
+  $(window).on('load', function () {
+    removePreloader();
+  });
+
+  //show skip loading link if loading takes too long 
+  $(function() {
+    $("#skip-loading").delay(5000).fadeIn(500);
+  });
+  
 })(window.jQuery);
